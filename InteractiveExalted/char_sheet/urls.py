a1d998540c314f3f7719245f44ef942248a39c1f
Leopold Olsson <bobbzorzen@gmail.com>
2017-10-22 01:02:25 +0200

    Added base for charm spellcards

--- InteractiveExalted/char_sheet/urls.py
+++ InteractiveExalted/char_sheet/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^cards/$', views.cards, name='cards'),
     url(r'^$', views.index, name='index')
 ]
--- InteractiveExalted/char_sheet/views.py
+++ InteractiveExalted/char_sheet/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'char_sheet/home.html')
+
+def cards(request):
+    return render(request, 'char_sheet/cards.html')
